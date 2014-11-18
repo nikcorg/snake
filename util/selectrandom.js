b370b3c6a50be666d10743d055b7dd104b940216
Niklas Lindgren <nikc@iki.fi>
2014-11-18 18:45:16 +0200

    Use randomize in selectrandom

--- util/selectrandom.js
+++ util/selectrandom.js
@@ -1,8 +1,8 @@
+var randomize = require("./randomize");
+
 module.exports = selectrandom.selectrandom = selectrandom;
 
 function selectrandom(arr, fn) {
-    var idx = Math.round(Math.random() * (arr.length - 1));
-
-    return fn(arr[idx]);
+    return fn(arr[randomize(arr.length)]);
 }
 
