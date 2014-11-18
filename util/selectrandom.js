d8a2dd99bc8ec7635de2a70235812230fe2aad8f
Niklas Lindgren <nikc@iki.fi>
2014-11-18 19:58:32 +0200

    Fix empty array

--- util/selectrandom.js
+++ util/selectrandom.js
@@ -3,6 +3,9 @@ var randomize = require("./randomize");
 module.exports = selectrandom.selectrandom = selectrandom;
 
 function selectrandom(arr, fn) {
+    if (arr.length < 1) {
+        return undefined;
+    }
     return fn(arr[randomize(arr.length)]);
 }
 
