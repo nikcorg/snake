f3a473fd82fad5e0ffad826360448a59ecc996fa
Niklas Lindgren <nikc@iki.fi>
2014-11-18 22:07:39 +0200

    Update and util test

--- test/util/and.js
+++ test/util/and.js
@@ -2,45 +2,52 @@ var test = require("tape");
 var and = require("../../util/and");
 
 test("util/and", function (t) {
+    t.test("export", function (t) {
+        t.plan(2);
+        t.equal(typeof and, "function", "exports function");
+        t.equal(typeof and.and, "function", "exports redundant api");
+    });
+
     t.test("input is passed", function (t) {
+        t.plan(2);
+
         var inp = true;
 
         and(function (i) {
-            t.equal(inp, i);
+            t.equal(i, inp, "input matches");
+            return i;
         }, function (i) {
-            t.equal(inp, i);
+            t.equal(i, inp, "input matches");
+            return i;
         })(inp);
-
-        t.end();
     });
 
     t.test("lazy", function (t) {
+        t.plan(1);
+
         var inp = false;
 
         and(function (i) {
-            t.equal(inp, i);
+            t.pass("left should be invoked");
+            return i;
         }, function (i) {
-            console.log("right");
-            t.equal(inp, i);
+            t.fail("right not be invoked");
+            return i;
         })(inp);
-
-        t.end();
     });
 
     t.test("returns", function (t) {
         function identity(_) { return _; }
 
         t.test("truthy", function (t) {
+            t.plan(1);
             var inp = "truthy";
             t.equal(inp, and(identity, identity)(inp));
-            t.end();
         });
         t.test("falsy", function (t) {
+            t.plan(1);
             var inp = 0;
             t.equal(inp, and(identity, identity)(inp));
-            t.end();
         });
-
-        t.end();
     });
 });
