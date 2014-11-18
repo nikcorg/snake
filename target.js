c858888629f26e94d27ffe8ba69609c394b86f03
Niklas Lindgren <nikc@iki.fi>
2014-11-18 16:17:42 +0200

    Randomise colour when target is super

--- target.js
+++ target.js
@@ -1,4 +1,6 @@
+var debug = require("debug")("target");
 var randomize = require("./randomize");
+var pad = require("./util/pad");
 
 module.exports = target.target = target.make = target;
 
@@ -13,11 +15,22 @@ function target(canvas) {
         height: xSize,
         width: ySize,
         update: update,
-        poison: false
+        poison: false,
+        super: false
     };
 
     function update() {
-        _target.color = _target.poison ? "#f00" : "#0f0";
+        if (_target.poison) {
+            _target.color = "#a00";
+        } else if (_target.super) {
+            _target.color = "#" +
+                pad(Math.round(Math.random() * 255).toString(16), 2, "0") +
+                pad(Math.round(Math.random() * 255).toString(16), 2, "0") +
+                pad(Math.round(Math.random() * 255).toString(16), 2, "0");
+            debug("randomized color %s", _target.color);
+        } else {
+            _target.color = "#0a0";
+        }
     }
 
     return _target;
