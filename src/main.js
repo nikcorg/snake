52cc6fd466afa56252b203bf18829932ad253fd2
Niklas Lindgren <nikc@iki.fi>
2014-11-19 13:25:02 +0200

    Enable or disable debug based on process.env

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-var debug = require("debug");
-
-debug.disable();
+if (/preview/.test(process.env.NODE_ENV)) {
+    require("debug").enable(process.env.DEBUG);
+} else {
+    require("debug").disable();
+}
 
 require("util/domready")(require("./game").bind(undefined, document));
